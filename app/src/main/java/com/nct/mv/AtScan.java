c5cc80c2d6c092e9d4059429418978c074d29288
thanhoanghai <thanhoanghai@gmail.com>
2015-05-12 20:15:09 +0700

    * update code

--- app/src/main/java/com/nct/mv/AtScan.java
+++ app/src/main/java/com/nct/mv/AtScan.java
@@ -9,35 +9,6 @@ import com.nct.utils.Utils;
 
 import thh.com.mycouper.R;
 
-public class AtScan extends Activity implements ZBarScannerView.ResultHandler {
-	private ZBarScannerView mScannerView;
-
-	@Override
-	public void onCreate(Bundle state) {
-		super.onCreate(state);
-		mScannerView = new ZBarScannerView(this);
-		setContentView(mScannerView);
-	}
-
-	@Override
-	public void onResume() {
-		super.onResume();
-		mScannerView.setResultHandler(this);
-		mScannerView.startCamera();
-	}
-
-	@Override
-	public void onPause() {
-		super.onPause();
-		mScannerView.stopCamera();
-	}
-
-	@Override
-	public void handleResult(Result rawResult) {
-		Toast.makeText(this, "Contents = " + rawResult.getContents() +
-				", Format = " + rawResult.getBarcodeFormat().getName(), Toast.LENGTH_SHORT).show();
-		mScannerView.startCamera();
-	}
-}
+public class AtScan extends Activity {
 
 }
