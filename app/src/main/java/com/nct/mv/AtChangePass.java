f65deba6d7e7f8530525ed823a143bb2ed8a5b93
thanhoanghai <thanhoanghai@gmail.com>
2015-05-15 22:56:39 +0700

    update code + add activity change pass

---
+++ app/src/main/java/com/nct/mv/AtChangePass.java
@@ -0,0 +1,20 @@
+package com.nct.mv;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.nct.utils.Debug;
+import com.nct.utils.Utils;
+
+import thh.com.mycouper.R;
+
+public class AtChangePass extends AtBase {
+	private static final String tag = "AtChangePass";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.at_change_pass);
+
+	}
+}
