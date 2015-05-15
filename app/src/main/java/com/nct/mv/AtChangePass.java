4ff7d3345f746018af80a76c03a47f8039b8dac2
thanhoanghai <thanhoanghai@gmail.com>
2015-05-15 23:28:01 +0700

    * update code

--- app/src/main/java/com/nct/mv/AtChangePass.java
+++ app/src/main/java/com/nct/mv/AtChangePass.java
@@ -2,6 +2,8 @@ package com.nct.mv;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 import com.nct.utils.Debug;
 import com.nct.utils.Utils;
@@ -11,10 +13,27 @@ import thh.com.mycouper.R;
 public class AtChangePass extends AtBase {
 	private static final String tag = "AtChangePass";
 
+	private Button bntClose,bntChange;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.at_change_pass);
+
+		bntChange = (Button)findViewById(R.id.change_pass_bt_change);
+		bntChange.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
+		bntClose = (Button) findViewById(R.id.change_pass_bt_close);
+		bntClose.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
 
 	}
 }
