3be2a93e72b4c22f9ea2eda363348c66e71ab444
thanhoanghai <thanhoanghai@gmail.com>
2015-05-15 23:38:12 +0700

    * update code change pass

--- app/src/main/java/com/nct/mv/AtChangePass.java
+++ app/src/main/java/com/nct/mv/AtChangePass.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.nct.utils.Debug;
 import com.nct.utils.Utils;
@@ -15,10 +16,20 @@ public class AtChangePass extends AtBase {
 
 	private Button bntClose,bntChange;
 
+	private EditText edtCurrentpass,edtNewpass,edtConfirmpass;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.at_change_pass);
+
+		initTopbar(getString(R.string.change_pass));
+		setTopbarBtLeftImage(R.drawable.icon_back);
+		setTopbarBtRightVisible(View.INVISIBLE);
+
+		edtCurrentpass = (EditText) findViewById(R.id.change_pass_edt_currentpass);
+		edtNewpass = (EditText) findViewById(R.id.change_pass_edt_newpass);
+		edtConfirmpass = (EditText) findViewById(R.id.change_pass_edt_confirm);
 
 		bntChange = (Button)findViewById(R.id.change_pass_bt_change);
 		bntChange.setOnClickListener(new View.OnClickListener() {
