f1bbb2db96826331bcbfc66b011d153447d4debc
thanhoanghai <thanhoanghai@gmail.com>
2015-05-16 15:32:38 +0700

    * update code update status item segment card detail

--- app/src/main/java/com/nct/mv/AtCardDetail.java
+++ app/src/main/java/com/nct/mv/AtCardDetail.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import com.nct.constants.GlobalInstance;
 import com.nct.model.MemberCardObject;
@@ -18,6 +20,12 @@ public class AtCardDetail extends AtBase {
 	private MemberCardObject memberCard;
 
 	private ImageView imgIcon;
+
+	private RadioButton bnt1,bnt2,bnt3;
+
+	private TextView imgCode;
+	private ImageView imgFront,imgBack;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +46,49 @@ public class AtCardDetail extends AtBase {
 
 		imgIcon = (ImageView) findViewById(R.id.at_card_detail_img);
 		initImageLoader();
-		displayImage(imgIcon,memberCard.company_logo);
+		displayImage(imgIcon, memberCard.company_logo);
 
+		bnt1= (RadioButton) findViewById(R.id.layout_segment_bt1);
+		bnt1.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				setVisibleItemSegment(0);
+			}
+		});
+		bnt2= (RadioButton) findViewById(R.id.layout_segment_bt2);
+		bnt2.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				setVisibleItemSegment(1);
+			}
+		});
+		bnt3= (RadioButton) findViewById(R.id.layout_segment_bt3);
+		bnt3.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				setVisibleItemSegment(2);
+			}
+		});
+
+		imgCode = (TextView) findViewById(R.id.card_detail_img_idcode);
+		imgFront = (ImageView) findViewById(R.id.card_detail_img_front);
+		imgBack = (ImageView) findViewById(R.id.card_detail_img_back);
+	}
+
+	private void setVisibleItemSegment(int index)
+	{
+		if(index==0)
+			setStatusItem(View.VISIBLE,View.INVISIBLE,View.INVISIBLE);
+		else if(index ==1)
+			setStatusItem(View.INVISIBLE,View.VISIBLE,View.INVISIBLE);
+		else if(index==2)
+			setStatusItem(View.INVISIBLE,View.INVISIBLE,View.VISIBLE);
+	}
+
+	private void setStatusItem(int status1,int status2,int status3)
+	{
+		imgCode.setVisibility(status1);
+		imgFront.setVisibility(status2);
+		imgBack.setVisibility(status3);
 	}
 }
