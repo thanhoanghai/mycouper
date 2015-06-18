48ee2c0da7bd906d006cc9542f677fefffb39307
xuannghi <xuannghi0387@gmail.com>
2015-06-18 19:14:57 +0700

    Zoom Image card

--- app/src/main/java/com/nct/mv/AtPreviewImage.java
+++ app/src/main/java/com/nct/mv/AtPreviewImage.java
@@ -1,21 +1,18 @@
 package com.nct.mv;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.ImageView;
 
 import com.nct.constants.Constants;
-import com.nct.utils.Debug;
-import com.nct.utils.Utils;
+import com.nct.customview.TouchImageView;
 
 import thh.com.mycouper.R;
 
 public class AtPreviewImage extends AtBase {
 
 	private String linkImage = "";
-	private ImageView img;
+	private TouchImageView img;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +34,7 @@ public class AtPreviewImage extends AtBase {
 			linkImage = extras.getString(Constants.AT_PREVIEW_ID_LINK_IMAGE);
 			// and get whatever type user account id is
 		}
-		img = (ImageView) findViewById(R.id.preview_image_img);
+		img = (TouchImageView) findViewById(R.id.preview_image_img);
 		initImageLoader();
 		if(!TextUtils.isEmpty(linkImage))
 			displayImage(img,linkImage);
